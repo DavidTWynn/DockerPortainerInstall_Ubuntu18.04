eab43c2a970244c2e2818d684c3363a4dbe5777d
David Wynn <50935601+DavidTWynn@users.noreply.github.com>
2019-07-19 15:03:51 -0400

    Update DockerPortainerInstall.sh
    Needed to do some editing to make it not prompt you to install. It still asks for password which is fine. Added su - ${USER} at the end of the script because having it before would stop the script from running.

--- DockerPortainerInstall.sh
+++ DockerPortainerInstall.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 echo "Updating repo and installing docker"
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
 echo "Adding current user to docker group"
-sudo apt install docker-ce
+sudo apt install -y docker-ce
 sudo usermod -aG docker ${USER}
-su - ${USER}
-exit
 
 echo "Installing portainer" 
-docker volume create portainer_data
-docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
-docker ps -a 
+sudo docker volume create portainer_data
+sudo docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+sudo docker ps -a 
 echo "Finished" 
+su - ${USER} 
