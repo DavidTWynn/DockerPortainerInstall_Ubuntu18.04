6ae701dfbf06ae0279118f018e5524414db04050
David Wynn <50935601+DavidTWynn@users.noreply.github.com>
2019-07-20 17:47:51 -0400

    changed name to portainer, restart always

--- DockerPortainerInstall.sh
+++ DockerPortainerInstall.sh
@@ -11,7 +11,7 @@ sudo usermod -aG docker ${USER}
 
 echo "Installing portainer" 
 sudo docker volume create portainer_data
-sudo docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+sudo docker run -d -p 9000:9000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
 sudo docker ps -a 
 echo "Finished" 
 su - ${USER} 
